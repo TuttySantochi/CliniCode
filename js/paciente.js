7eaf8486b4035b4a4111c0296e5947f75bd2c450
TuttySantochi <tuttysantochi69@gmail.com>
2023-07-19 23:42:03 -0300

    trabajando en el localstorage

--- js/paciente.js
+++ js/paciente.js
@@ -1,8 +1,8 @@
 const d = document;
-const SelectEspecialista = d.getElementById("SelectEspecialista");
-const SelectFecha = d.getElementById("SelectFecha");
-const SelectHorario = d.getElementById("SelectHorario");
-const DatosConsulta = d.getElementById("DatosConsulta");
+let SelectEspecialista = d.getElementById("SelectEspecialista");
+let SelectFecha = d.getElementById("SelectFecha");
+let SelectHorario = d.getElementById("SelectHorario");
+let DatosConsulta = d.getElementById("DatosConsulta");
 
 
 const campos = {
@@ -43,27 +43,44 @@ DatosConsulta.addEventListener("change", e => {
 })
 
 
-let form = {};
-  form.Especialista = SelectEspecialista.value;
-  form.Fecha = SelectFecha.value;
-  form.Horario = SelectHorario.value;
-  form.DatosConsulta = DatosConsulta.value;
+//let form = {};
+//  form.Especialista = SelectEspecialista.value;
+//  form.Fecha = SelectFecha.value;
+//  form.Horario = SelectHorario.value;
+//  form.DatosConsulta = DatosConsulta.value;
 
 const submitFormulario = d.getElementById("submitFormulario");
 
-
-guardarTurno(form);
-
-
-function guardarTurno(form) {
-    let f = JSON.stringify(form);
-    localStorage.setItem('form', f);
+function guardarTurno() {
+	
+	let SelectEspecialista = d.getElementById("SelectEspecialista").value;
+	let SelectFecha = d.getElementById("SelectFecha").value;
+	let SelectHorario = d.getElementById("SelectHorario").value;
+	let DatosConsulta = d.getElementById("DatosConsulta").value;
+   // let f = JSON.stringify(form);
+   // localStorage.setItem('form', f);
+   let form;  
+   if (localStorage.getItem('form') == null) {
+	form = [];
+} else {
+	//form = JSON.parse(localStorage.getItem("form"));
+}
+form.push({
+	SelectEspecialista: SelectEspecialista,
+	SelectFecha: SelectFecha,
+	SelectHorario: SelectHorario,
+	DatosConsulta: DatosConsulta
+});
+localStorage.setItem("form", JSON.stringify(form))
+	
 }
 
 submitFormulario.addEventListener('click', (e) => {
 	e.preventDefault();
 	
 	if(campos.Especialista && campos.Horario && campos.Fecha && campos.DatosConsulta){
+		guardarTurno();
+
 		formulario.reset();
         Swal.fire('¡Su turno ha sido registrado con éxito!')
 
@@ -72,11 +89,7 @@ submitFormulario.addEventListener('click', (e) => {
         campos.Horario = false;
         campos.Especialista = false;
 
-        console.log('f');
-
-
-        console.log("hola");
-
+       
 	} else {
         Swal.fire('Asegurese de haber completado todos los campos.')
         console.log("chau");
